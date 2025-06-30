67361654aed76781aaaf3e934d77c36bf9c10d0b
alhu2591 <143001551+alhu2591@users.noreply.github.com>
2025-07-01 00:36:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+
+set -e  # وقف عند أول خطأ
+
+# إنشاء بيئة افتراضية (اختياري بس منصوح فيه)
+python3.11 -m venv .venv
+source .venv/bin/activate
+
+# تحديث pip وتنصيب الحزم
+pip install --upgrade pip
 pip install -r requirements.txt
-npx playwright install --with-deps
+
+# تثبيت متصفحات Playwright الخاصة بـ Python
+python -m playwright install
